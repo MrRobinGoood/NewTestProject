04ce2c4e854d943e706c1dd9d530a9a2ef67a244
Andrey Bartenev <59123372+MrRobinGoood@users.noreply.github.com>
2022-10-01 17:28:04 +0400

    Create default door

--- Source/NewTestProject/NewTestProject.h
+++ Source/NewTestProject/NewTestProject.h
@@ -3,3 +3,17 @@
 #pragma once
 
 #include "CoreMinimal.h"
+
+
+
+
+
+/*
+
+// Begin and End Overlap Events for our DoorProxVolume
+UFUNCTION()
+void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+UFUNCTION()
+void OnOverlapEnd(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+*/
